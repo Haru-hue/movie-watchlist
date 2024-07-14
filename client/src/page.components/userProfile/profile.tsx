958c8c09cd94587367ed76a1046ac2faec6cca8a
Josh <ukojoshy@gmail.com>
2024-07-14 12:42:47 +0100

    Update profile.tsx

--- client/src/page.components/userProfile/profile.tsx
+++ client/src/page.components/userProfile/profile.tsx
@@ -68,13 +68,13 @@ export const UserProfilePage = () => {
               isLoading={loading}
               setFiles={onProfileChange}
             />
-            <div className="mt-4">
-              <h3 className="mb-1.5 text-2xl font-semibold text-black dark:text-white">
+            <div className="mt-6">
+              <h3 className="text-2xl font-semibold text-black dark:text-white">
                 {localUser?.name}
               </h3>
-              {/* <h3 className="mb-1.5 text-2xl font-semibold text-black dark:text-white">
-                {localUser?.username}
-              </h3> */}
+              <h3 className="text-md text-black dark:text-white">
+                @{localUser?.username}
+              </h3>
             </div>
           </div>
         </div>
